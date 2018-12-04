9090813d6ad62cd7154c6abaef0cfd43f63c8698
cruzeddie78 <ecruz19@mail.csuchico.edu>
2018-12-03 17:12:14 -0800

    made a class for student

---
+++ student.h
@@ -0,0 +1,15 @@
+#ifndef _STUDENT_H_
+#define _STUDENT_H_
+#include <string>
+using namespace std;
+
+class student
+{
+  private:
+    string firstname;
+    string lastname;
+  public:
+    string setName(string firstname,setname);
+    string fullName();
+};
+#endif
