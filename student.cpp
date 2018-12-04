96373e8e3b9db9cdb6b72bf2f216dfa0bbab3fbc
cruzeddie78 <ecruz19@mail.csuchico.edu>
2018-12-03 17:32:45 -0800

    yay

---
+++ student.cpp
@@ -0,0 +1,24 @@
+#include "student.h"
+using namespace std;
+#include <string>
+
+void student::setName(string first, string last)
+  {
+    string firstname = first;
+    string lastname =  last;
+  }
+string student::fullName()
+{
+  string full;
+  string first;
+  string space;
+  string last;
+  full.append(first);
+  full.append(space);
+  full.append(last);
+  
+  return full;
+
+}
+
+
